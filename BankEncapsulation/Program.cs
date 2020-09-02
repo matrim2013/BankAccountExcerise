50a1b8258d3fc2fcd7ea05b9e88f220fbd25e8f8
Lewis S <simpkinslewisa@gmail.com>
2020-09-02 15:42:06 -0400

    Added Personal Info Class

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -8,15 +8,21 @@ namespace BankEncapsulation
         {
             var userAccount = new BankAccount(500);
             
-
             userAccount.Deposit();
-            Console.WriteLine("   ");
+            
             userAccount.Withdraw();
-            Console.WriteLine("  ");
+            
             Console.WriteLine($"Your new balance is {userAccount.GetBalance(), 0:c} ");
+            // In the Main method, create an instance of the PersonalInformation class and ask the user to input their phone number, 
+            //then display the newly formatted phone number(NumberFormatted) to the user
+
+            var userAbe = new PersonalInformation();
+            Console.WriteLine("What is your phone number?");
+            userAbe.NumberFormatted = Console.ReadLine();
+            Console.WriteLine($"Your phone number has been recorded as {userAbe.NumberFormatted}");
 
 
-          
-        }
+
+    }
     }
 }
