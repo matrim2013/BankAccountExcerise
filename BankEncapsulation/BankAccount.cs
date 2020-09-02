cd261c1a1204ce021f5c854cd93a9865a396153a
Lewis S <simpkinslewisa@gmail.com>
2020-09-02 12:57:54 -0400

    Fist Stab

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount() { }
+        private double Balance = 1000;
+        public void Deposit (double d)
+        {
+            Balance += d;
+        }
+        public double GetBalance()
+        {
+            return Balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userAccount = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+            double depositAmount = double.Parse(Console.ReadLine());
+
+            userAccount.Deposit(depositAmount);
+
+            Console.WriteLine($"Your new balance is {userAccount.GetBalance()} ");
+
+          
+        }
+    }
+}
